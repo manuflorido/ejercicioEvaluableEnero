4be710827d75adf1dd60fc5af6f01f43324ad27f
manuflorido <60346060+manuflorido@users.noreply.github.com>
2022-01-17 11:17:48 +0100

    Añadido el archivo arrayDB.php, sin acabar

---
+++ arrayDB.php
@@ -0,0 +1,16 @@
+<?php
+//Creación de los arrays necesarios para el ejercicio
+$vueloDestino=array(
+    "Vueling-AE12" => "Milán",
+    "Volotea-EA21" => "Milán",
+    "Iberia-WE44" => "Milán",
+    "Ryanair-EW44" => "Milán",
+    "Vueling-ZK87" => "Milán",
+    "Volotea-UA21" => "Milán",
+    "Iberia-WH44" => "Milán",
+    "Ryanair-EL00" => "Milán",
+    "Vueling-AE12" => "Milán",
+    "Volotea-EA21" => "Milán"
+)
+
+?>
